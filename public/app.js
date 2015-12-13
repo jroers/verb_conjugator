4df9fe915d007908388d7357c2af624f5f4bb389
jroers <joe.roers@gmail.com>
2015-12-13 14:12:24 -0800

    Added verb search bar

--- public/app.js
+++ public/app.js
@@ -4,29 +4,33 @@ $(document).ready(function() {
 	var url;
 	var dataToUse = {};
 
-	$(".btn-primary").click(function handler() {
-		var infinitive = $(this).text().toLowerCase();
-		url = "/api/verbs/" + infinitive;
-		$.ajax({
-			method: "GET",
-			url: url,
-			success: function (data) {
-				console.log(data);
-				// je = data.tense.present.je;
-				// tu = data.tense.present.tu;
-				// il = data.tense.present.il;
-				// nous = data.tense.present.nous;
-				// vous = data.tense.present.vous;
-				// ils = data.tense.present.ils;
-				// $(".conjugation.je").text(je);
-				// $(".conjugation.tu").text(tu);
-				// $(".conjugation.il").text(il);
-				// $(".conjugation.nous").text(nous);
-				// $(".conjugation.vous").text(vous);
-				// $(".conjugation.ils").text(ils);
-				renderConjugation(data);
-			}
-		});
+	$("input").keydown(function handler(event) {
+		if (event.which === 13) {
+			console.log("Hey there");
+			// var infinitive = $(this).val().toLowerCase();
+			// url = "/api/verbs/" + infinitive;
+			// $.ajax({
+			// 	method: "GET",
+			// 	url: url,
+			// 	success: function (data) {
+			// 		console.log(data);
+			// 		// je = data.tense.present.je;
+			// 		// tu = data.tense.present.tu;
+			// 		// il = data.tense.present.il;
+			// 		// nous = data.tense.present.nous;
+			// 		// vous = data.tense.present.vous;
+			// 		// ils = data.tense.present.ils;
+			// 		// $(".conjugation.je").text(je);
+			// 		// $(".conjugation.tu").text(tu);
+			// 		// $(".conjugation.il").text(il);
+			// 		// $(".conjugation.nous").text(nous);
+			// 		// $(".conjugation.vous").text(vous);
+			// 		// $(".conjugation.ils").text(ils);
+			// 		renderConjugation(data);
+			// 	}
+			// });
+		}
+		
 	});
 });
 
