fa66955e43de6fe0dfe34f0d3a2db12b85a0c998
jroers <joe.roers@gmail.com>
2015-12-15 10:44:03 -0800

    Added hard-coded data to app.js for lists

--- public/app.js
+++ public/app.js
@@ -10,6 +10,51 @@ var tenseCount = 0;
 //update this variable with the maxiumum number of tenses stored.
 var tenseMax = 2;
 
+var erList = {
+	name: "Regular ER verbs - Present tense",
+	description: "All of the site's regular ER verbs",
+	verbs: []
+};
+
+var irList = {
+	name: "Regular IR verbs - Present tense",
+	description: "All of the site's regular IR verbs",
+	verbs: []
+};
+
+var reList = {
+	name: "Regular RE verbs - Present tense",
+	description: "All of the site's regular RE verbs",
+	verbs: []
+};
+
+$.ajax({
+	method: "GET",
+	url: "/api/verbs",
+	success: function (data) {
+		data.forEach(function (element) {
+			if (element.family === "er" && element.tense.present.irregular === false) {
+				console.log(element.infinitive);
+				dataToUse._id = element._id;
+				dataToUse.tense = "present";
+				erList.verbs.push(dataToUse);
+			} else if (element.family === "ir" && element.tense.present.irregular === false) {
+				dataToUse._id = element._id;
+				dataToUse.tense = "present";
+				irList.verbs.push(dataToUse);
+			} else if (element.family === "re" && element.tense.present.irregular === false) {
+				dataToUse._id = element._id;
+				dataToUse.tense = "present";
+				reList.verbs.push(dataToUse);
+			}
+		});
+		console.log(erList);
+		console.log(irList);
+		console.log(reList);
+	}
+});
+
+
 $(document).ready(function() {
 	console.log("sanity check. JS is working");
 
