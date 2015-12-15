a263b48cb04dcfd48ba62454c6222d9341a1f360
jroers <joe.roers@gmail.com>
2015-12-15 13:51:54 -0800

    Updated list schema to resolve error, added dropdown menu in the lists html page

--- models/list.js
+++ models/list.js
@@ -1,5 +1,6 @@
 var mongoose = require("mongoose");
-var Verb = mongoose.model('Verb', VerbSchema);
+var Schema = mongoose.Schema;
+var Verb = require("./verb.js");
 
 var ListSchema = new mongoose.Schema({
 	name: String,
